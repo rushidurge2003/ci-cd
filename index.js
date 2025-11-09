14b8370e3bdb87039332c72cc8ac67d71b2f7d06
rushidurge2003 <rushikeshdurge7794@gmail.com>
2025-11-09 18:39:08 +0530

    add .gitignore file and create server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const name = "Jay Shree Ram";
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
